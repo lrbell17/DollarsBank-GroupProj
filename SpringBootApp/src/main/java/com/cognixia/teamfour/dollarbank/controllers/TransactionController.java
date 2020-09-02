91494f80083d0d8d63f23434f1d29b0083397448
lrbell17 <56008909+lrbell17@users.noreply.github.com>
2020-09-02 12:43:25 -0700

    Allow cross-origin sharing to react app

--- SpringBootApp/src/main/java/com/cognixia/teamfour/dollarbank/controllers/TransactionController.java
+++ SpringBootApp/src/main/java/com/cognixia/teamfour/dollarbank/controllers/TransactionController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cognixia.teamfour.dollarbank.models.Transaction;
 import com.cognixia.teamfour.dollarbank.services.TransactionService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/transaction")
 public class TransactionController {
