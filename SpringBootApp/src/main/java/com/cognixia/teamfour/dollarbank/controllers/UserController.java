e2f7c929e31f9d14e9acde46dcd63d23d2d88205
andrewstich <andrew.stich@hotmail.com>
2020-09-02 13:28:39 -0700

    Changed DB

--- SpringBootApp/src/main/java/com/cognixia/teamfour/dollarbank/controllers/UserController.java
+++ SpringBootApp/src/main/java/com/cognixia/teamfour/dollarbank/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.cognixia.teamfour.dollarbank.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
