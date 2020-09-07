9049b7cd429ce90e48066bf7fe5b05bc9456a883
andrewstich <andrew.stich@hotmail.com>
2020-09-07 13:11:14 -0700

    added transactionService

---
+++ React Front End/src/services/TransactionService.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const trans_url = "http://localhost:8080/transaction";
+
+class TransactionService {
+
+    getAllUserTransactions = (userId) => {
+        return axios.get(`${trans_url}/user/${userId}`);
+    }
+
+    getAllAccountTransactions = (accountId) => {
+        return axios.get(`${trans_url}/acct/${accountId}`);
+    }
+
+    createTransaction = (type, user_id, account_id, amount, start_balance, end_balance, time_stamp) => {
+        return axios.post(`${trans_url}/create`, {
+            type: type,
+            userId: user_id,
+            accountId: account_id,
+            amount: amount,
+            startBalance: start_balance,
+            endBalance: end_balance,
+            timeStamp: time_stamp
+        });
+    }
+
+    deleteTransaction = (id) => {
+        return axios.delete(`${trans_url}/delete/${id}`);
+    }
+}
+
+export default new TransactionService();
